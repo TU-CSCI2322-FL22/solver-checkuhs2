df08afe009dc727094dcb442c2cb3ac58c61f9cc
Cole McGuire <cmcguir1@trinity.edu>
2022-10-31 13:19:45 -0500

    Created types

--- Checkers.hs
+++ Checkers.hs
@@ -1,1 +1,24 @@
+data Player = Red | Black | None deriving (Eq,Show)
+data Kind = Emperor | Peasent deriving (Eq,Show)
 
+type Piece = (Player,Kind)
+type Board = [[Piece]]
+type Coordinate = (Integer,Integer)
+type Move = [(Coordinate,Coordinate)]
+type GameState = (Player,Board)
+
+instance Show Board where
+  show board = undefined
+  
+
+checkWinner :: GameState -> Player
+
+makeMove :: GameState -> Move -> GameState
+
+makeLegalMove :: GameState -> Move -> GameState
+
+isValidMove :: GameState -> Move -> Boolean
+
+getValidMoves :: GameState -> [Move]
+
+let defaultBoard = undefined
