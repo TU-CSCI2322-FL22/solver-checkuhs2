0ce519c6be384d33fb827bd2c89bbc7354e7d4fd
2021-08-24 <jadkins@janus01.cs.trinity.edu>
2022-11-04 13:23:59 -0500

    getPieceAtIndex complete

--- Checkers.hs
+++ Checkers.hs
@@ -6,27 +6,38 @@ type Board = [[Maybe Piece]]
 type Coordinate = (Integer,Integer)
 type Move = [(Coordinate,Coordinate)]
 type GameState = (Player,Board)
-  
+    
 prettyShow :: GameState -> String
+prettyShow = undefined
 
 checkWinner :: GameState -> Maybe Player
+checkWinner = undefined
 
 makeMove :: GameState -> Move -> Maybe GameState
+makeMove = undefined
 
 makeLegalMove :: GameState -> Move -> GameState
+makeLegalMove = undefined
 
-isValidMove :: GameState -> Move -> Boolean
+getPieceAtIndex :: GameState -> Coordinate -> Maybe Piece
+getPieceAtIndex (player,board) (x,y) = 
+  let xIndex = if ((y % 2) == 0) then (x + 1)/2 else x/2
+  in drop xIndex-1 (take xIndex (drop y-1 (take y board)))
+
+isValidMove :: GameState -> Move -> Bool
+isValidMove (player,board) move = False
 
 getValidMoves :: GameState -> [Move]
+getValidMoves (player,board) = undefined
 
-let defaultBoard = 
+defaultBoard = 
   [
-    [Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent)]
-    [(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing]
-    [Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent)]
-    [Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing]
-    [Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing]
-    [(Red,Peasent),Nothing,(Red,Peasent),Nothing,(Red,Peasent),Nothing,(Red,Peasent),Nothing]
-    [Nothing,(Red,Peasent),Nothing,(Red,Peasent),Nothing,(Red,Peasent),Nothing,(Red,Peasent)]
+    [Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent)],
+    [(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing],
+    [Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent),Nothing,(Black,Peasent)],
+    [Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing],
+    [Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing],
+    [(Red,Peasent),Nothing,(Red,Peasent),Nothing,(Red,Peasent),Nothing,(Red,Peasent),Nothing],
+    [Nothing,(Red,Peasent),Nothing,(Red,Peasent),Nothing,(Red,Peasent),Nothing,(Red,Peasent)],
     [(Red,Peasent),Nothing,(Red,Peasent),Nothing,(Red,Peasent),Nothing,(Red,Peasent),Nothing]
   ]
