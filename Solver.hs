c39a6c4c55f4f97ccdf82e792a9968cd9c90149c
2021-08-24 <jadkins@janus01.cs.trinity.edu>
2022-11-14 12:42:14 -0600

    added comments

--- Solver.hs
+++ Solver.hs
@@ -2,8 +2,15 @@ module Solver where
 
 import Checkers
 
+--Function "who will win" that takes a Game and returns an Outcome. 
+--Considers every valid move, the resulting game state, 
+--and chooses the move with the best outcome for the current player.
 predictedWinner :: GameState -> Outcome
 predictedWinner = undefined
 
+--Function "best move" that takes a Game and return the best Move.
+--Given a game state, search for a move that can force a win for the current player. 
+--Failing that, return a move that can force a tie for the current player. 
+--This will involve recursively searching through the game states that result from that move. 
 bestMove :: GameState -> Move
 bestMove = undefined
