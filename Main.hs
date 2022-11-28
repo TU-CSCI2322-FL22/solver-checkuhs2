c0e084c7b51216e0b621f14f744acf332906dbb1
Ayden Tanner <atanner@trinity.edu>
2022-11-28 13:15:44 -0600

    command line something

--- Main.hs
+++ Main.hs
@@ -1,9 +1,23 @@
 module Main where
 
+import System.Directory
+import System.IO
+
 import Checkers
 import InputOutput
 import Solver
 
 main = do
-    gs <- loadGame "text.txt"
+    --args <- getArgs **will be used**
+    gs <- getBoardFromFile
     printUglyShow gs
+
+getBoardFromFile :: IO GameState
+getBoardFromFile = do
+    putStr $ "Board File: "
+    hFlush stdout
+    maybeFile <- getLine
+    isValidFile <- doesFileExist maybeFile
+    if isValidFile 
+    then loadGame maybeFile
+    else getBoardFromFile
