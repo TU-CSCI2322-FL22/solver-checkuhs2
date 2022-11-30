73e43b97b0b3b21f08aa2a6d8cca32bf89ba44e5
Ayden Tanner <atanner@trinity.edu>
2022-11-30 13:07:25 -0600

    parseMove stub

--- Main.hs
+++ Main.hs
@@ -2,15 +2,34 @@ module Main where
 
 import System.Directory
 import System.IO
+import System.Console.GetOpt
 
 import Checkers
 import InputOutput
 import Solver
 
+data Flag = Winner | Depth String | Help | Mover String | Verbose | Interactive deriving (Eq,Show)
+
+options :: [OptDescr String]
+options = [ Option ['w'] ["winner"] (NoArg Winner) "Print out the best move, using an exhaustive search (no cut-off depth)."
+           ,Option ['d'] ["depth"] (RegArg Depth "#") "Use this number as a cutoff depth, instead of your default."
+           ,Option ['h'] ["help"] (NoArg Help)
+           ,Option ['m'] ["move"] (RegArg Mover "#")
+          ]
+
+getMove :: [Flag] -> Maybe Move
+getMove ((Mover x):_) = Nothing --TODO
+getMove (_:xs) = getMove xs
+getMove [] = Nothing
+
+parseMove :: String -> Maybe Move
+parseMove = undefined
+
 main = do
     --args <- getArgs **will be used**
-    gs <- getBoardFromFile
-    printUglyShow gs
+    let (flags,inputs,error) = getOpt Permute options args
+    if Help `elem` flags || (not $ null error)
+    then putStrLn $ usageInfo "Usage: "
 
 getBoardFromFile :: IO GameState
 getBoardFromFile = do
