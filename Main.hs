03dc1beee94895e70d4e7134eda8bad45ac628f7
Cole McGuire <cmcguir1@trinity.edu>
2022-12-01 19:42:25 -0600

    Working on move parser

--- Main.hs
+++ Main.hs
@@ -7,3 +7,9 @@ import Solver
 main = do
     gs <- loadGame "text.txt"
     printUglyShow gs
+
+--Ideas for parse move
+--1. Filter all non-number characters then use pairs of those numbers to construct coordinates
+
+parseMove :: String -> Maybe Move
+parseMove str = 
